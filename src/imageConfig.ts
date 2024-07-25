fd45aa94720101a4387e95b9d1d579422fe1beda
Volodymyr Voronkin <you@example.com>
2024-07-25 11:34:31 +0200

    change image config

--- src/imageConfig.ts
+++ src/imageConfig.ts
@@ -1,30 +1,30 @@
 export const imageConfig = {
     backGroundMobile: {
-        src: '/bg-mobile-light.jpg',
+        src: 'bg-mobile-light.jpg',
         width: 480,
         height: 200,
         alt: 'background'
     },
     backgroundDesktop: {
-        src: '/bg-desktop-light.jpg',
+        src: 'bg-desktop-light.jpg',
         width: 1920,
         height: 600,
         alt: 'background'
     },
     moon: {
-        src: '/icon-moon.svg',
+        src: 'icon-moon.svg',
         width: 20,
         height: 20,
         alt: 'moon'
     },
     delete: {
-        src: '/icon-cross.svg',
+        src: 'icon-cross.svg',
         width: 15,
         height: 15,
         alt: 'delete toDo'
     },
     checked: {
-        src: '/icon-check.svg',
+        src: 'icon-check.svg',
         width: 15,
         height: 10,
         alt: 'checked'
